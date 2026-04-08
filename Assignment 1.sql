2f6295a777b2bf10a4846eace8cd62b7d1df7944
SOUJANYA KALITA <62724436+SOUJU07@users.noreply.github.com>
2026-04-08 20:25:15 +0530

    Add files via upload

---
+++ Assignment 1.sql
@@ -0,0 +1,88 @@
+SELECT COUNT(*) FROM superstore.superstore_orders;
+
+
+-- 1- write a sql to get all the orders where customers name has "a" as second character and "d" as fourth character (58 rows)
+
+select *
+from superstore_orders
+where customer_name LIKE '_a_d%';
+
+
+-- 2- write a sql to get all the orders placed in the month of dec 2020 (352 rows) 
+
+select *
+from superstore_orders
+where order_date between '2020-12-01 00:00:00' and '2020-12-31 00:00:00';
+
+-- 3- write a query to get all the orders where ship_mode is neither in 'Standard Class' 
+-- nor in 'First Class' and ship_date is after nov 2020 (944 rows)
+
+SELECT *
+FROM superstore_orders
+where ship_mode NOT IN ('Standard Class','First Class') and ship_date > '2020-11-30';
+
+
+-- 4- write a query to get all the orders where customer name neither start with "A" and nor ends with "n" (9815 rows)
+
+SELECT COUNT(*)
+from superstore_orders
+where customer_name NOT LIKE 'A%n';
+
+
+-- 5- write a query to get all the orders where profit is negative (1871 rows)
+
+SELECT COUNT(*)
+FROM superstore_orders
+where profit<0;
+
+-- 6- write a query to get all the orders where either quantity is less than 3 or profit is 0 (3348)
+
+select count(*)
+from superstore_orders
+where quantity <3 or profit =0;
+
+
+-- 7- your manager handles the sales for South region and he wants you to create a report of all 
+-- the orders in his region where some discount is provided to the customers (815 rows)
+
+select *
+from superstore_orders
+where region = 'South' and discount >0;
+
+
+-- 8- write a query to find top 5 orders with highest sales in furniture category 
+
+SELECT *
+from superstore_orders
+where category = "Furniture"
+order by sales DESC
+limit 5;
+
+
+-- 9- write a query to find all the records in technology and furniture category for the orders placed in the year 2020 only (1021 rows)
+
+SELECT count(*) 
+from superstore_orders
+where category IN ('Technology','Furniture')
+and order_date between '2020-01-01' and '2020-12-31';
+
+-- 10-write a query to find all the orders where order date is in year 2020 but ship date is in 2021 (33 rows)
+
+SELECT *
+FROM superstore_orders
+where YEAR(order_date)=2020 and year(ship_date)=2021;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
